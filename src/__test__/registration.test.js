db135ffed14c799902e7e1bb246f660662dfbe38
Swapnil Karande <swapnilk38@hotmail.com>
2021-07-22 11:14:48 +0530

    adding more tests

--- src/__test__/registration.test.js
+++ src/__test__/registration.test.js
@@ -1,8 +1,9 @@
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import { configure, mount, render, shallow } from "enzyme";
-import Registration from "../components/registration";
+import { configure, mount, shallow } from "enzyme";
 import { BrowserRouter as Router } from 'react-router-dom';
 import { act } from "react-dom/test-utils";
+import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import Registration from "../components/registration";
 
 describe('Registration form', () => {
     configure({ adapter: new Adapter() });
@@ -18,5 +19,18 @@ describe('Registration form', () => {
             const wrapper = mount(<Router><Registration /></Router>);
             expect(wrapper.find('input').length).toEqual(7);
         });
+    });
+
+    it("should not submit if required fields are empty", async () => {
+        await act(async () => render(<Router><Registration /></Router>));
+        fireEvent.submit(screen.getByRole("button"));
+        expect(await screen.findAllByText("Name is required")).toHaveLength(1);
+        expect(await screen.findAllByText("Age is required")).toHaveLength(1);
+        expect(await screen.findAllByText("Date of birth is required")).toHaveLength(1);
+        expect(await screen.findAllByText("Profession is required")).toHaveLength(1);
+        expect(await screen.findAllByText("Locality is required")).toHaveLength(1);
+        expect(await screen.findAllByText("Number of guests is required")).toHaveLength(1);
+        expect(await screen.findAllByText("Address is required")).toHaveLength(1);
+        cleanup();
     });
 });
